b042f446016774572102881092553cde5a8586fe
NtwananoNdhlovu <St10227753@rcconnect.edu.za>
2023-04-26 14:34:11 +0000

    just created the ingredirnts class succesfully

--- TheRecipeApp/Program.cs
+++ TheRecipeApp/Program.cs
@@ -2,3 +2,13 @@
 using System.Collections.Generic;
 
 
+//this is a ingredient class, stores the Name, Quantity and unit
+namespace TheRecipeApp
+{
+    class Ingredient
+    {
+        public string Name { get; set; }
+        public double Quantity { get; set; }
+        public string Unit { get; set; }
+    }
+}
